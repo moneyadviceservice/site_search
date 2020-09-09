cac38b05ad7da47997f9505c3d45dd978b55848b
Abraham Kiggundu <abraham@thekiggys.com>
2020-09-09 15:05:37 +0100

    [TP-11719] updated the test for a successful search and associated VCR

--- spec/site_search/adapter/algolia_spec.rb
+++ spec/site_search/adapter/algolia_spec.rb
@@ -70,85 +70,10 @@ RSpec.describe SiteSearch::Adapter::Algolia do
         'Looking after your dependants in retirement'
       end
 
-      # rubocop:disable Metrics/LineLength
-      let(:results) do
-        {
-          results: [
-            {
-              title: '<br>Looking</br> <br>after</br> <br>your</br> <br>dependants</br> <br>in</br> <br>retirement</br>',
-              description: 'How to make sure that <br>your</br> partner and/or children will be provided for when you retire.',
-              link: '/en/articles/looking-after-your-dependants-in-retirement',
-              raw: {
-                title: 'Looking after your dependants in retirement',
-                description: 'How to make sure that your partner and/or children will be provided for when you retire.'
-              }
-            }
-          ],
-          total_results: 1,
-          page: 0,
-          number_of_pages: 1,
-          per_page: 10,
-          query: query
-        }
-      end
-      # rubocop:enable Metrics/LineLength
-
-      it 'returns the highlighted results' do
+      it 'returns the expected results' do
         VCR.use_cassette('retirement_page', match_requests_on: [:body]) do
-          expect(algolia.search(query)).to eq(results)
-        end
-      end
-    end
 
-    context 'when searching a page without highlight results' do
-      let(:query) do
-        'mortgages'
-      end
-
-      let(:result_without_description) do
-        {
-          title: '<br>Mortgages</br>',
-          description: nil,
-          link: '/en/categories/mortgages',
-          raw: {
-            title: 'Mortgages',
-            description: nil
-          }
-        }
-      end
-
-      it 'returns the highlighted results' do
-        VCR.use_cassette('mortgages', match_requests_on: [:body]) do
-          expect(algolia.search(query)[:results].first)
-            .to eq(result_without_description)
-        end
-      end
-    end
-
-    context 'when searching page with description but not highlighted' do
-      let(:query) do
-        'where free advice'
-      end
-
-      # rubocop:disable Metrics/LineLength
-      let(:result_without_description) do
-        {
-          title:
-            '<br>Where</br> to go to get <br>free</br> debt <br>advice</br>',
-          description: "Free debt advice and help locator tool. Use our tool to find a debt advice service near you that's accredited by the Money Advice Service.",
-          link: '/en/tools/debt-advice-locator',
-          raw: {
-            title: 'Where to go to get free debt advice',
-            description: "Free debt advice and help locator tool. Use our tool to find a debt advice service near you that's accredited by the Money Advice Service."
-          }
-        }
-      end
-      # rubocop:enable Metrics/LineLength
-
-      it 'returns the highlighted results' do
-        VCR.use_cassette('debt_advice', match_requests_on: [:body]) do
-          expect(algolia.search(query)[:results])
-            .to include(result_without_description)
+          expect(algolia.search(query)).to include(:results => array_including(hash_including(:title, :link, :description)))
         end
       end
     end
